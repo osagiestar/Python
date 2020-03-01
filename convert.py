a1391fcb091bd48bdbcdd562622253722800a5ff
osagiestar <okoedooc@yahoo.com>
2020-03-01 07:31:37 +0000

    Initial commit

---
+++ convert.py
@@ -0,0 +1,16 @@
+x = 1 # int
+y = 2.8 # float
+z = 1j # complex
+
+#convert x to float
+a = float(x)
+
+#converts y to int
+b = int(y)
+
+#converts x to complex
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
