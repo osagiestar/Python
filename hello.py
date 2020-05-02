170d8b28510cc69bbc1542f8893a5ca6537d0f1d
osagiestar <okoedooc@yahoo.com>
2020-05-02 15:01:59 +0100

    Python practice

---
+++ hello.py
@@ -0,0 +1,16 @@
+print ("Hello World!!!")
+
+if 5 > 2:
+    print("Five is greater than 2")
+
+x = 3
+y = 8
+z = x * y
+print(z)
+
+x = "awesome"
+
+def myfunc():
+    print("Python is " + x)
+
+myfunc()
